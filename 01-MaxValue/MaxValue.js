1a26b358b35fb5dfa17fbb047c8c46214d5a4adc
Hengers3012 <hengersrosario.m@gmail.com>
2023-10-02 21:54:44 -0400

    feat: agregando comentarios

--- 01-MaxValue/MaxValue.js
+++ 01-MaxValue/MaxValue.js
@@ -1,19 +1,22 @@
 function MaxValue(shares) {
   if (shares.length < 2) {
+    // No se pueden realizar operaciones de compra y venta con menos de 2 precios
     return 0;
   }
 
-  let minPrice = shares[0];
-  let maxProfit = 0;
+  let minPrice = shares[0]; // Inicializamos el precio mínimo con el primer elemento
+  let maxProfit = 0; // Inicializamos la ganancia máxima en 0
 
   for (let i = 1; i < shares.length; i++) {
     const currentPrice = shares[i];
     const potentialProfit = currentPrice - minPrice;
 
+    // Se actualiza la ganancia máxima si se encuentra una ganancia mayor
     if (potentialProfit > maxProfit) {
       maxProfit = potentialProfit;
     }
 
+    // Se actualiza el precio mínimo si se encuentra un precio más bajo
     if (currentPrice < minPrice) {
       minPrice = currentPrice;
     }
